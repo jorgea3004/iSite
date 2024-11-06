6e0388cc094eba3b3fa0dfb34eeb54d3d547353b
Jorge González <152451951+jgonzalezDS@users.noreply.github.com>
2024-11-06 12:08:21 -0600

    Primer Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iSite</title>
+</head>
+<body>
+    <h1>Sitio Test iSite</h1>
+    <div>Pagina inicial.</div>
+    <div><?=date('Y-m-d H:i')?></div>
+</body>
+</html>
